c890a182d563a72a6aee001fd9811e91d0d65d33
Kraggle09 <redstoneweeklypodcast@gmail.com>
2025-11-29 15:18:47 -0600

    Added hair metadata

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ def main():
     skin.save('skin.png')
 
 def randomSkin():
-    img = Image.open(f"assets/{random.choice(baseList)}.png")
+    img = Image.new("RGBA", (64, 64), (0, 0, 0, 0))
+    img = overlay(img, random.choice(baseList), False)
     img = overlay(img, random.choice(eyeList))
+    img = overlay(img, random.choice(hairList))
     return img
 
 def tint(image, coordinate, color=(255, 255, 255, 255)):
@@ -20,13 +22,18 @@ def tint(image, coordinate, color=(255, 255, 255, 255)):
     overlay.putpixel(coordinate, color)
     return Image.alpha_composite(image, overlay)
 
-def overlay(base, layer):
-    with open(f"assets/{layer}.json") as file:
-        metadata = json.load(file)
+def overlay(base, layer, shouldTint=True):
     layer_image = Image.open(f"assets/{layer}.png")
-    tint_color = random_color(200)
-    for coord in metadata['coordinates']:
-        layer_image = tint(layer_image, coord, tint_color)
+    if shouldTint:
+        with open(f"assets/{layer}.json") as file:
+            metadata = json.load(file)
+        tint_color = random_color(200)
+        for coord in metadata['coordinates']:
+            if len(coord) == 2:
+                layer_image = tint(layer_image, coord, tint_color)
+            else:
+                for rangepixels in range(coord[0], coord[1]+1):
+                    layer_image = tint(layer_image, (rangepixels, coord[2]), tint_color)
     return Image.alpha_composite(base, layer_image)
 
 def random_color(alpha):
