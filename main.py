b151e748bff749bfd05a74313da47db3fb939fb0
Kraggle09 <redstoneweeklypodcast@gmail.com>
2025-11-30 11:09:01 -0600

    Implemented mask system

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from PIL import Image
 import random
 import json
+import os
 
 baseList = ['base0']
 hairList = ['hair0', 'hair1']
-eyeList = ['eye0', 'eye1']
+eyeList = ['eye1']
 
 def main():
     skin = randomSkin()
@@ -17,24 +18,24 @@ def randomSkin():
     img = overlay(img, random.choice(hairList))
     return img
 
-def tint(image, coordinate, color=(255, 255, 255, 255)):
-    overlay = Image.new("RGBA", (64, 64), (0, 0, 0, 0))
-    overlay.putpixel(coordinate, color)
-    return Image.alpha_composite(image, overlay)
+def tint(image, color=(255, 255, 255, 255)):
+    layer_image = Image.open(f"assets/{image}.png")
+    tint_layer = Image.new("RGBA", (64, 64), color)
+    if os.path.exists(f"assets/{image}_mask.png"):
+        mask = Image.open(f"assets/{image}_mask.png").convert("L")
+        return Image.composite(tint_layer, layer_image, mask)
+    else:
+        r, g, b, a = layer_image.split()
+        base_rgb = Image.merge("RGB", (r, g, b))
+        tint_rgb = Image.new("RGB", layer_image.size, color[:3])
+        blended_rgb = Image.blend(base_rgb, tint_rgb, color[3] / 255.0)
+        return Image.merge("RGBA", (*blended_rgb.split(), a))
 
-def overlay(base, layer, shouldTint=True):
-    layer_image = Image.open(f"assets/{layer}.png")
-    if shouldTint:
-        with open(f"assets/{layer}.json") as file:
-            metadata = json.load(file)
-        tint_color = random_color(200)
-        for coord in metadata['coordinates']:
-            if len(coord) == 2:
-                layer_image = tint(layer_image, coord, tint_color)
-            else:
-                for rangepixels in range(coord[0], coord[1]+1):
-                    layer_image = tint(layer_image, (rangepixels, coord[2]), tint_color)
-    return Image.alpha_composite(base, layer_image)
+def overlay(base, layer, should_tint=True):
+    if should_tint:
+        return Image.alpha_composite(base, tint(layer, random_color(225)))
+    else:
+        return Image.alpha_composite(base, Image.open(f"assets/{layer}.png"))
 
 def random_color(alpha):
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), alpha)
